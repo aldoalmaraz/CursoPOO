d01c3ab07e2934c943f5a01b5821759803c5ffd8
Aldo Almaraz <doalfull@gmail.com>
2022-04-04 17:04:05 -0500

    Creando objetos en phyton leccion numero 25

--- Python/Car.py
+++ Python/Car.py
@@ -1,6 +1,11 @@
+from Account import Account
+
 class Car:
     id = int
     license = str
-    driver = str
+    driver = Account("","")
     passegenger = int
     
+    def __init__(self, license, drive):
+        self.license = license
+        self.drive=drive
